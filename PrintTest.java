366cfdcd82489f9bc021b30a4c2ddc99461f5bb1
son63un <80902151+son63un@users.noreply.github.com>
2021-03-18 22:42:20 +0900

    Add files via upload

---
+++ PrintTest.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+	System.out.println("한동석");	
+	System.out.print("남자");
+	System.out.printf(double,"args)
+	}
+	
+
+}
